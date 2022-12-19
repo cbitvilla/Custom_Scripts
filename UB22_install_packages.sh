01745759fe2721f5a1b2dd23f8f1257cf506c0db
Charles Villa <93799140+cbitvilla@users.noreply.github.com>
2022-12-19 08:58:26 -0500

    added banners
    added banners to the different sections of the script

--- UB22_install_packages.sh
+++ UB22_install_packages.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
 # Will need to add a way to detwemin if you are root or not
 # If you are NOT root, then change into root
+echo -e "################################\n"
 echo -e "RUNNING SYSTEM UPDATE FOR UBUNTU 22.04...\n"
+echo -e "################################\n"
 apt update && apt upgrade -y
 echo -e "\n"
 
+echo -e "################################\n"
 echo -e "INSTALLING NECESSARY PACKAGES...\n"
+echo -e "################################\n"
+
 apt install -y apt-transport-https \
 libc6 \
 libstdc++6 \
@@ -21,11 +26,15 @@ openssh-server \
 vim
 echo -e "\n"
 
+echo -e "################################\n"
 echo "INSTALLING nala...\n"
+echo -e "################################\n"
 apt install -y nala
 echo -e "\n"
 
+echo -e "################################\n"
 echo -e "INSTALLING DOCKER-CE..."
+echo -e "################################\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
@@ -35,14 +44,18 @@ systemctl enable docker
 docker --version
 echo -e "\n"
 
+echo -e "################################\n"
 echo -e "INSTALLING DOCKER-COMPOSE...\n"
+echo -e "################################\n"
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker compose version
 echo -e "\n"
 
+echo -e "################################\n"
 echo -e "INSTALLING ZSH & OH-MY-ZSH...\n"
+echo -e "################################\n"
 apt install -y zsh
 zsh --version
 apt install -y git-core curl fonts-powerline
